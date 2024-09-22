5791225210a891501376381851d2a5e0092bd449
Arghava Mousanezhad Zangalani <amousane@volvocars.com>
2024-09-22 19:53:42 +0200

    protect dashboard route

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,6 @@
-import React, { Suspense, lazy, useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { Suspense, lazy, useState, useEffect } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -15,11 +16,21 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    } else {
+      history.push("/");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -31,13 +42,15 @@ const App = () => {
               <Route path="/auth">
                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              <Route path="/dashboard" component={DashboardApp} />
+              <Route path="/dashboard">
+                {!isSignedIn ? <Redirect to="/" /> : <DashboardApp />}
+              </Route>
               <Route path="/" component={MarketingApp} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
